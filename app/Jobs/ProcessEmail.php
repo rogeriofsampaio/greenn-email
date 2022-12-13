dde4eb18d93c70bd8ff491ec162d99b1d0a5325d
graweb1@gmail.com <graweb1@gmail.com>
2022-12-13 11:08:24 -0300

    project send email

---
+++ app/Jobs/ProcessEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\Email;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Mail;
+
+class ProcessEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $emailData;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($emailData)
+    {
+        $this->emailData = $emailData;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->emailData['email'])->send(new Email($this->emailData));
+    }
+}
